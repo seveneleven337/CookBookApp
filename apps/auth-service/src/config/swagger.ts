f2949ee053b85056e5abf1151abbbe68de443b53
seveneleven337 <solidxnake_6@hotmail.com>
2026-03-18 22:37:23 +0200

    auth-service: Implement Swagger documentation for Auth Service API

---
+++ apps/auth-service/src/config/swagger.ts
@@ -0,0 +1,31 @@
+import swaggerJSDoc, { Options } from 'swagger-jsdoc';
+
+const options: Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Auth Service API',
+      version: '1.0.0',
+      description: 'Authentication microservice API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:4000/api/auth',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+
+  apis: process.env.NODE_ENV === 'production' ? ['dist/routes/*.js'] : ['src/routes/*.ts'],
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
