e5d6f213b1f977fecdfcc76bd0d63794d0484458
vhlahti <henri.veholahti@gmail.com>
2026-03-28 00:33:57 +0200

    web: Set login page as landing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,5 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace('/homepage');
-  }, [router]);
-
-  return null;
+  redirect('/login');
 }
