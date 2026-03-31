d8a5aad31ea5fcae4bef8d314376933f7e68082d
seveneleven337 <solidxnake_6@hotmail.com>
2026-03-31 21:51:25 +0300

    web: Improve data logic related to auth service on the web application, including types, zustand and tanstack

---
+++ apps/web/src/data/react-query/useAuth.ts
@@ -0,0 +1,39 @@
+import { useMutation } from '@tanstack/react-query';
+import { loginUser, registerUser } from '@/data/lib/auth-service';
+import { useUserStore } from '@/data/store/authStore';
+
+export function useAuth() {
+  const setUser = useUserStore((state) => state.setUser);
+  const clearUser = useUserStore((state) => state.clearUser);
+
+  // Login
+  const loginMutation = useMutation({
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      setUser(data); // data ya es User
+    },
+  });
+
+  // Register
+  const registerMutation = useMutation({
+    mutationFn: registerUser,
+    onSuccess: (data) => {
+      setUser(data);
+    },
+  });
+
+  const logout = () => {
+    clearUser();
+    // Aquí puedes llamar al endpoint de logout si existe
+  };
+
+  return {
+    login: loginMutation.mutate,
+    register: registerMutation.mutate,
+    logout,
+    isLoggingIn: loginMutation.isPending,
+    isRegistering: registerMutation.isPending,
+    isError: loginMutation.isError || registerMutation.isError,
+    error: (loginMutation.error || registerMutation.error) as Error | null,
+  };
+}
