3d4e89c94297d28e0ba1d8c0bf84c7a840c55366
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-07 16:10:02 +0300

    web: Add navBar navigation on LandingPage

---
+++ apps/web/src/components/NavBar.tsx
@@ -0,0 +1,60 @@
+'use client';
+import { useUserStore } from '@/data/store/authStore';
+import Link from 'next/link';
+
+const brandClass = 'text-xl font-bold text-brand';
+const navItemsClass =
+  'text-lg font-semibold text-nav-item hover:text-gray-900 transition-colors underline decoration-transparent hover:decoration-black decoration-offset-[2px] duration-200';
+
+/*
+ * TODO: - Implement button feature on NavBar elements ( Breakfast, Vegan, Dessert, Salad) to filter recipes by category
+ * Improve logo * icon
+ */
+export default function NavBar() {
+  const { user, clearUser } = useUserStore();
+
+  const handleLogout = async () => {
+    clearUser();
+  };
+
+  return (
+    <div className="w-screen  border-b-2 border-border-bg z-50 px-8 py-2 flex items-center justify-between backdrop-blur-sm">
+      <div className={brandClass}>COOKBOOK</div>
+      <div className="flex items-center justify-baseline gap-4">
+        <button className={navItemsClass}>Breakfast</button>
+        <button className={navItemsClass}>Vegan</button>
+        <button className={navItemsClass}>Dessert</button>
+        <button className={navItemsClass}>Salad</button>
+      </div>
+      <div className="flex items-center gap-4">
+        {!user ? (
+          <>
+            <Link href="/login" className="inline-block">
+              <button className="rounded-full px-4 py-3 text-nav-item font-bold text-base border-2 border-transparent hover:border-nav-item duration-200">
+                Login
+              </button>
+            </Link>
+
+            <Link href="/register" className="inline-block">
+              <button className="rounded-full bg-form-btn-bg px-4 py-3 text-white font-bold text-base hover:bg-form-btn-bg-hover">
+                Register
+              </button>
+            </Link>
+          </>
+        ) : (
+          <div className="flex items-center gap-3">
+            <span className="text-lg font-semibold sm:inline text-nav-item pr-2">
+              {'Hi, ' + user.name}
+            </span>
+            <button
+              onClick={handleLogout}
+              className="rounded-full bg-form-btn-bg px-4 py-3 text-white font-bold text-base hover:bg-form-btn-bg-hover"
+            >
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
