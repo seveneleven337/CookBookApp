5d88e7a5af08e10d503ae2da4dddebbb1bc8f8c7
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-07 16:08:15 +0300

    web: remove random api and left side bar component

--- apps/web/src/app/api/meals/random/route.ts
+++
@@ -1,7 +0,0 @@
-import { getRandomMeal } from '@/data/lib/recipe-api';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const meal = await getRandomMeal();
-  return NextResponse.json(meal);
-}
