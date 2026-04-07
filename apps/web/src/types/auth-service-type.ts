fb9d17b976e0dd2b8198008b4fcf4d7fcaa7a1bf
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-07 08:41:26 +0300

    web: Remove register and login response types

--- apps/web/src/types/auth-service-type.ts
+++ apps/web/src/types/auth-service-type.ts
@@ -5,27 +5,7 @@ export type RegisterPayload = {
   password: string;
 };
 
-export type RegisterResponse = {
-  user: {
-    id: number;
-    name: string;
-    lastName: string;
-    email: string;
-  };
-  token?: string;
-};
-
 export type LoginPayload = {
   email: string;
   password: string;
-};
-
-export type LoginResponse = {
-  token: string;
-  user: {
-    id: number;
-    name: string;
-    lastName: string;
-    email: string;
-  };
 };
