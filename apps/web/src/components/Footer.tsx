7a4f44bcd6284aff5e56230b9a33928992db713e
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-08 19:48:55 +0300

    web: implement footer component

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div>
-      <footer className="w-screen py-8 text-center border-t border-gray-200 bg-footer-bg bg-linear-to-tl from-footer-bg to-tertiary">
+      <footer className="w-screen border-t-3 border-border-bg z-50 px-8 py-10 flex items-center justify-center ">
         <div className="flex items-center justify-center mb-2">
           <div className="flex flex-col items-end gap-2">
             <p className="text-gray-600 text-sm whitespace-nowrap ">2025. All rights reserved.</p>
