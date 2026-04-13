ffd2eb490a0ef083d7e6de018930ca1182fab6e3
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-13 18:46:07 +0300

    web: Update endpoint to production

--- apps/web/src/data/lib/endpoints.ts
+++ apps/web/src/data/lib/endpoints.ts
@@ -2,6 +2,7 @@
  * This file defines constants for API endpoints used in the application.
  */
 
+// Public API endpoints
 export const BASE_URL = 'https://www.themealdb.com/api/json/v1/1';
 export const RECIPE_API_URL = process.env.RECIPE_API_URL ?? 'https://cookbook.fi/api/recipes';
 export const AUTH_API_URL = process.env.AUTH_API_URL ?? 'https://cookbook.fi/api/auth';
