3a06e25693d95cb9adf94aeadbe117be44846f15
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-13 21:53:17 +0300

    web: Redirect logged in user to home page

--- apps/web/src/app/landing/page.tsx
+++ apps/web/src/app/landing/page.tsx
@@ -4,8 +4,18 @@ import NavBar from '@/components/NavBar';
 import MainFrame from './MainFrame';
 import RecipesFrame from './RecipesFrame';
 import CategoryFrame from './CategoryFrame';
+import { useUserStore } from '@/data/store/authStore';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function LandingPage() {
+  const router = useRouter();
+  const user = useUserStore((state) => state.user);
+
+  useEffect(() => {
+    if (user?.token) router.push('/home');
+  }, [user, router]);
+
   return (
     <div className="flex flex-col items-center justify-center w-full gap-4">
       {/* nav bar */}
