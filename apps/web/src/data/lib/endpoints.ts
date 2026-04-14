3f76d1452cc9a523bcd230dd83c81b3c554af5fd
Kalle Närhi <me@kallenarhi.com>
2026-04-14 10:47:38 +0300

    web: Fix Auth & Recipe URL endpoint selecting for test and production environments

--- apps/web/src/data/lib/endpoints.ts
+++ apps/web/src/data/lib/endpoints.ts
@@ -3,5 +3,5 @@
  */
 
 export const BASE_URL = 'https://www.themealdb.com/api/json/v1/1';
-export const RECIPE_API_URL = 'https://cookbook.fi/api/recipes';
-export const AUTH_API_URL = 'https://cookbook.fi/api/auth';
+export const RECIPE_API_URL = process.env.RECIPE_API_URL ?? 'https://cookbook.fi/api/recipes';
+export const AUTH_API_URL = process.env.AUTH_API_URL ?? 'https://cookbook.fi/api/auth';
