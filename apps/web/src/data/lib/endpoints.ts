21d60ecb26cacfa27c16641059b8286cf8d8e1ab
Kalle Närhi <me@kallenarhi.com>
2026-04-14 10:31:56 +0300

    web: Update endpoint variables

--- apps/web/src/data/lib/endpoints.ts
+++ apps/web/src/data/lib/endpoints.ts
@@ -3,7 +3,5 @@
  */
 
 export const BASE_URL = 'https://www.themealdb.com/api/json/v1/1';
-export const RECIPE_API_URL =
-  process.env.RECIPE_SERVICE_URL ?? 'http://localhost:5001/api/recipes';
-export const AUTH_API_URL =
-  process.env.AUTH_SERVICE_URL ?? 'http://localhost:4000/api/auth';
+export const RECIPE_API_URL = 'https://cookbook.fi/api/recipes';
+export const AUTH_API_URL = 'https://cookbook.fi/api/auth';
