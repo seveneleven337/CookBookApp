2d91918844415fdf71e26d062cc609350c883a34
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-16 18:56:29 +0300

    web: Fix issue with frame size

--- apps/web/src/components/RecipesFrame.tsx
+++ apps/web/src/components/RecipesFrame.tsx
@@ -17,15 +17,9 @@ export default function RecipesFrame({ title, subtitle, meals }: RecipeFramProps
           <div className="text-3xl text-primary font-semibold">{title}</div>
           <div className="flex font-normal text-sm text-text-subtitle">{subtitle}</div>
         </div>
-
-        <div className="w-full max-w-6xl flex flex-col items-center gap-y-5 py-4 overflow-x-hidden">
-          <div className="flex justify-center gap-5 max-w-full">
-            {meals?.slice(0, 3).map((meal) => (
-              <MealCard key={meal.idMeal} meal={meal} />
-            ))}
-          </div>
-          <div className="flex justify-center gap-5  max-w-full">
-            {meals?.slice(3, 6).map((meal) => (
+        <div className="w-full flex justify-center">
+          <div className="w-full max-w-5xl grid gap-5 py-4 overflow-x-hidden md:grid-cols-3 justify-center items-center">
+            {meals.slice(0, 6).map((meal) => (
               <MealCard key={meal.idMeal} meal={meal} />
             ))}
           </div>
