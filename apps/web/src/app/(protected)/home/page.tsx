e470f02aef9083d7d47ae5185d89566cd71e0a94
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-16 18:12:28 +0300

    web: Implement home page

--- apps/web/src/app/(protected)/home/page.tsx
+++ apps/web/src/app/(protected)/home/page.tsx
@@ -1,3 +1,29 @@
+'use client';
+import Footer from '@/components/Footer';
+import NavBar from '@/components/NavBar';
+
+import CategoryFrame from '@/app/landing/CategoryFrame';
+import RecipesFrame from '@/components/RecipesFrame';
+import { useRecipes } from '@/data/react-query/useRecipe';
+import MainFrame from '@/components/MainFrame';
+
 export default function HomePage() {
-  return <div>This will be the home page content</div>;
+  const { data: meals } = useRecipes();
+  return (
+    <div className="flex flex-col items-center justify-center w-full gap-4">
+      {/* nav bar */}
+      <NavBar />
+      {/* landing content */}
+      <MainFrame />
+      {/* categories */}
+      <CategoryFrame />
+      {/* 6 random recipes */}
+      <RecipesFrame
+        title={'Latest From The Kitchen'}
+        subtitle={'*Good food is the foundation for genuine happiness'}
+        meals={meals || []}
+      />{' '}
+      <Footer />
+    </div>
+  );
 }
