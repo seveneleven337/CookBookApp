69d7a2af62e1987bd1e5158cc19f1359052fe996
Kalle Närhi <me@kallenarhi.com>
2026-04-16 11:22:45 +0300

    recipe-management: Add JWT token variable & package dependencys

--- apps/recipe-management-service/src/config/env.ts
+++ apps/recipe-management-service/src/config/env.ts
@@ -6,3 +6,4 @@ export const RECIPE_DB_USER = process.env.RECIPE_DB_USER!;
 export const RECIPE_DB_PASSWORD = process.env.RECIPE_DB_PASSWORD!;
 export const RECIPE_DB_HOST = process.env.RECIPE_DB_HOST || 'localhost';
 export const RECIPE_DB_PORT = Number(process.env.RECIPE_DB_PORT) || 5432;
+export const JWT_SECRET = process.env.JWT_SECRET!;
