916e7979b3127e0270ecadbdd5712190bbb99cdd
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-16 18:46:02 +0300

    web: add RecipePage with data fetching by id

--- apps/web/src/app/recipe/[id]/page.tsx
+++ apps/web/src/app/recipe/[id]/page.tsx
@@ -1,9 +1,19 @@
 'use client';
-import { useParams } from 'next/navigation';
+import Recipe from '@/components/recipe';
+import { useRecipeById } from '@/data/react-query/useRecipe';
+import { use } from 'react';
 
-export default function RecipePage() {
-  const params = useParams();
-  const id = params.id as string;
+export default function RecipePage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params);
+  const { data: meal, isLoading, isError } = useRecipeById(id);
 
-  return <div>Recipe ID: {id}</div>;
+  if (isLoading) {
+    return <div className="text-center p-6">Loading...</div>;
+  }
+
+  if (isError || !meal) {
+    return <div className="text-center p-6">Recipe not found.</div>;
+  }
+
+  return <Recipe meal={meal} />;
 }
