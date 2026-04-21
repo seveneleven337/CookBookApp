9869f0a8e2f7279dfa6a87d9acdd89f56cb3fdd6
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-21 16:36:17 +0300

    recipe-management: Update swagger path

--- apps/recipe-management-service/src/server.ts
+++ apps/recipe-management-service/src/server.ts
@@ -5,7 +5,7 @@ import { RECIPE_MANAGEMENT_SERVICE_PORT } from './config/env';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger';
 
-app.use('/api/recipes/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/recipes-management/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = RECIPE_MANAGEMENT_SERVICE_PORT;
 
