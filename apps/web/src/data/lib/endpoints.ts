61dca5caba8a903d62ccedf34e3474262691742c
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-21 15:09:57 +0300

    web: Add recommendation service endpoint

--- apps/web/src/data/lib/endpoints.ts
+++ apps/web/src/data/lib/endpoints.ts
@@ -3,5 +3,7 @@
  */
 
 export const BASE_URL = 'https://www.themealdb.com/api/json/v1/1';
-export const RECIPE_API_URL = process.env.RECIPE_API_URL ?? 'https://cookbook.fi/api/recipes';
-export const AUTH_API_URL = process.env.AUTH_API_URL ?? 'https://cookbook.fi/api/auth';
+export const RECIPE_API_URL = process.env.RECIPE_API_URL ?? 'http://localhost:5001/api/recipes';
+export const AUTH_API_URL = process.env.AUTH_API_URL ?? 'http://localhost:4000/api/auth';
+export const RECOMMENDED_RECIPES_URL =
+  process.env.RECOMMENDED_RECIPES_URL ?? 'http://localhost:8000/api/recommend';
