9545bbdaec234a82e4110cb6de780f50f03fadc1
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-21 15:10:55 +0300

    web: Implement recommended recipes, home page

--- apps/web/src/app/(protected)/home/page.tsx
+++ apps/web/src/app/(protected)/home/page.tsx
@@ -6,9 +6,13 @@ import CategoryFrame from '@/app/landing/CategoryFrame';
 import RecipesFrame from '@/components/RecipesFrame';
 import { useRecipes } from '@/data/react-query/useRecipe';
 import MainFrame from '@/components/MainFrame';
+import { useRecommendedRecipes } from '@/data/react-query/useRecommendRecipe';
+import { useUserStore } from '@/data/store/authStore';
 
 export default function HomePage() {
+  const user = useUserStore((state) => state.user);
   const { data: meals } = useRecipes();
+  const { data: recommendedMeals } = useRecommendedRecipes(user!, '53267');
   return (
     <div className="flex flex-col items-center justify-center w-full gap-4">
       {/* nav bar */}
@@ -19,9 +23,9 @@ export default function HomePage() {
       <CategoryFrame />
       {/* 6 recommended recipes */}
       <RecipesFrame
-        title={'Latest From The Kitchen'}
-        subtitle={'*Good food is the foundation for genuine happiness'}
-        meals={meals || []}
+        title={'Recommended For Your Kitchen'}
+        subtitle={'*Handpicked recipes just for you'}
+        meals={recommendedMeals || []}
         variant="recommended"
       />
       {/* 6 random recipes */}
