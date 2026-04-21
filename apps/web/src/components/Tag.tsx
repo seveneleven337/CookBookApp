716f1848fe586e0784d2f07586b91553fc863a49
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-21 14:18:50 +0300

    web: Add to tag tertiary variant

--- apps/web/src/components/Tag.tsx
+++ apps/web/src/components/Tag.tsx
@@ -1,14 +1,15 @@
 type TagProps = {
   text: string;
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary' | 'tertiary';
 };
 
 export default function Tag({ text, variant = 'primary' }: TagProps) {
-  const baseStyles = 'backdrop-blur text-xs font-semibold px-3 py-1 rounded-full w-fit';
+  const baseStyles = 'backdrop-blur text-xs font-semibold  rounded-full w-fit';
 
   const variants = {
-    primary: 'bg-card-tag-bg text-gray-700',
-    secondary: 'bg-tag-bg text-green-700',
+    primary: 'bg-card-tag-bg text-gray-700 px-3 py-1',
+    secondary: 'bg-tag-bg text-green-700 px-3 py-1',
+    tertiary: 'text-gray-800 px-0 py-1 text-input-text-active font-normal text-md',
   };
 
   return <span className={`${baseStyles} ${variants[variant]}`}>{text.trim()}</span>;
