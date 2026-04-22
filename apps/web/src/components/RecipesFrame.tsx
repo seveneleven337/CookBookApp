09cc722cc175ce28afe82716958c18ea46bcd015
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-22 11:40:34 +0300

    web: Replace variant recommended to myRecipe on recipeFrame

--- apps/web/src/components/RecipesFrame.tsx
+++ apps/web/src/components/RecipesFrame.tsx
@@ -1,12 +1,12 @@
 'use client';
 import MealCard from '@/components/MealCard';
-import { Meal } from '@/types/recipe-service-type';
+import { Meal } from '@/types/recipe-type';
 
 type RecipeFrameProps = {
   title: string;
   subtitle?: string;
   meals: Meal[];
-  variant?: 'default' | 'recommended';
+  variant?: 'default' | 'myRecipe';
 };
 
 export default function RecipesFrame({
@@ -17,14 +17,14 @@ export default function RecipesFrame({
 }: RecipeFrameProps) {
   if (!meals) return null;
   return (
-    <div className="flex flex-col w-full h-fit items-center justify-center  p-12 gap-6">
+    <div className="flex flex-col w-full h-fit items-center justify-between py-12 gap-6">
       <div className="w-full max-w-6xl flex flex-col gap-6">
         <div className="flex w-full h-fit items-center justify-center flex-col">
           <div className="text-3xl text-primary font-semibold">{title}</div>
           <div className="flex font-normal text-sm text-text-subtitle">{subtitle}</div>
         </div>
         <div className="w-full flex justify-center">
-          <div className="w-full max-w-5xl grid gap-5 py-4 overflow-x-hidden md:grid-cols-3 justify-center items-center">
+          <div className="w-full grid gap-5 py-4 overflow-x-hidden md:grid-cols-3 justify-center items-center">
             {meals.slice(0, 6).map((meal) => (
               <MealCard key={meal.idMeal + variant} meal={meal} variant={variant} />
             ))}
