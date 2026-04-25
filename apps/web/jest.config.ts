e6ed94d0b54ed6a7f8fa7c9e4c800e106b9ea083
seveneleven337 <solidxnake_6@hotmail.com>
2026-04-25 15:38:36 +0300

    web: Update jest module name mapper

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -8,7 +8,7 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
 
